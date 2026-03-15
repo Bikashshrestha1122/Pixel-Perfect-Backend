aeb19ecdeb79774f44fda86a3b0ca394e05d0308
Bikash Shrestha <bikaashshrestha090@gmail.com>
2026-03-15 13:37:13 +0545

    Update category.route.js

--- routes/category.route.js
+++ routes/category.route.js
@@ -35,10 +35,10 @@ Router.get('/', getCategories);
 
 Router.get('/:id', getCategoryById);
 
-Router.delete('/:id', authenticateAdmin, async (req, res) => {
+Router.delete('/:name', authenticateAdmin, async (req, res) => {
     try {
-        const category = await Category.findById(req.params.id);
-
+        let name = req.params.name.toLowerCase();
+        const category = await Category.findOne({ name });
         if (!category) {
             return res.status(404).json({ message: "Category not found" });
         }
@@ -47,14 +47,13 @@ Router.delete('/:id', authenticateAdmin, async (req, res) => {
             return res.status(400).json({ message: "Cannot delete category with products" });
         }
 
-        await Category.findByIdAndDelete(req.params.id);
-
+        await Category.findOneAndDelete({ name });
         res.status(200).json({ message: "Category deleted successfully" });
-
     } catch (error) {
         console.error("Error deleting category:", error);
         res.status(500).json({ message: "Server error" });
     }
+
 });
 
 
